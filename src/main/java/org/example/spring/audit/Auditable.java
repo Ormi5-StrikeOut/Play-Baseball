a474d622d455496071f77facea7afcdb368fc96c
2019112479kjh <jalirom@naver.com>
2024-08-28 15:06:43 +0900

    Feat: Message 엔티티 구현 #14
    - Message 엔티티 구현

---
+++ src/main/java/org/example/spring/audit/Auditable.java
@@ -0,0 +1,22 @@
+package org.example.spring.audit;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.sql.Timestamp;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Auditable {
+
+    @Column(nullable = false, insertable = false, updatable = false)
+    @CreatedDate
+    private Timestamp createAt;
+
+}
---
+++ src/main/java/org/example/spring/domain/message/Message.java
@@ -0,0 +1,32 @@
+package org.example.spring.domain.message;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.spring.audit.Auditable;
+import org.example.spring.domain.Member;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Builder
+@Table(name = "message")
+public class Message extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long messageId;
+
+    @Column(name = "message_content", nullable = false, columnDefinition = "TEXT")
+    private String messageContent;
+
+    // 연관관계
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "message_room_id", nullable = false)
+    private MessageRoom messageRoom;
+}
