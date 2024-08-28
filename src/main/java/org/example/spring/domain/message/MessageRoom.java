d59d39acadd77bd8d08f3453a2ebda834fd6d845
2019112479kjh <jalirom@naver.com>
2024-08-28 15:15:28 +0900

    Feat: Message 엔티티 구현 #14
    - Message 엔티티 구현

---
+++ src/main/java/org/example/spring/domain/message/MessageRoom.java
@@ -0,0 +1,28 @@
+package org.example.spring.domain.message;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.spring.audit.Auditable;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "messageRoom")
+@Builder
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MessageRoom extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long messageRoomId;
+
+    @OneToMany(mappedBy = "messageRoom", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @OrderBy("messageId DESC")
+    private Set<Message> messages = new LinkedHashSet<>();
+
+    public Message getLastMessage() {
+        return messages.stream().findFirst().orElse(null);
+    }
+}
