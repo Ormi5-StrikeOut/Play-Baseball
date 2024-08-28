e8a27eb564209a0f7c103a03404953f4295a7715
LLSNsssz <seungjang93@gmail.com>
2024-08-28 22:45:47 +0900

    Feat: MemberRepository 인터페이스 생성 및 JpaRepository 상속

---
+++ src/main/java/org/example/spring/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package org.example.spring.repository;
+
+import org.example.spring.domain.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
