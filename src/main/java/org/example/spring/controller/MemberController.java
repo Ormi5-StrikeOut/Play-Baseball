336b40234209b10c8443b6b23fd944fdf871124c
LLSNsssz <seungjang93@gmail.com>
2024-08-29 15:13:38 +0900

    Feat: 회원 가입 엔드포인트 구현
    - /join POST 매핑 추가

--- src/main/java/org/example/spring/controller/MemberController.java
+++ src/main/java/org/example/spring/controller/MemberController.java
@@ -1,7 +1,13 @@
 package org.example.spring.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.spring.domain.member.Member;
+import org.example.spring.domain.member.dto.MemberJoinRequestDto;
 import org.example.spring.service.MemberService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +18,9 @@ public class MemberController {
 
     private final MemberService memberService;
 
-
+    @PostMapping("/join")
+    public ResponseEntity<String> registerMember(@RequestBody MemberJoinRequestDto memberJoinRequestDto) {
+        memberService.registerMember(memberJoinRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body("회원가입에 성공했습니다.");
+    }
 }
