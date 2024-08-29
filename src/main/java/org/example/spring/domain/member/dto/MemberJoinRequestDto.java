49c6c3107c8d32560abc605c341785e04d64545e
LLSNsssz <seungjang93@gmail.com>
2024-08-29 14:37:37 +0900

    Refactor: MemberJoinRequestDto 롬복 어노테이션 수정
    - @ Getter 추가
    - @ Builder 추가
    - @ AllArgsConstructor(access = AccessLevel.PRIVATE) 추가
    - @ NoArgsConstructor(access = AccessLevel.PROTECTED) 추가
    - @ ToString 추가

--- src/main/java/org/example/spring/domain/member/dto/MemberJoinRequestDto.java
+++ src/main/java/org/example/spring/domain/member/dto/MemberJoinRequestDto.java
@@ -1,13 +1,22 @@
 package org.example.spring.domain.member.dto;
 
-import lombok.Data;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.example.spring.constants.Gender;
 import org.example.spring.domain.member.Member;
 
 /**
  * DTO for {@link Member}
  */
-@Data
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class MemberJoinRequestDto {
 
     String email;
