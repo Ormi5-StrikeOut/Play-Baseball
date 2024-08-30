53ef57e85c5f71a4bcb1cfaa17b3c7dc95ef3d3d
LLSNsssz <seungjang93@gmail.com>
2024-08-30 17:14:31 +0900

    Docs: CustomeUsernamePasswordAuthenticationProvider 자바독 작성

--- src/main/java/org/example/spring/security/CustomeUsernamePasswordAuthenticationProvider.java
+++ src/main/java/org/example/spring/security/CustomeUsernamePasswordAuthenticationProvider.java
@@ -11,6 +11,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+/**
+ * 사용자 정의 UsernamePasswordAuthenticationProvider 구현체 입니다.
+ * Spring Security에서 사용자 인증 과정을 처리합니다.
+ */
 @Component
 @RequiredArgsConstructor
 public class CustomeUsernamePasswordAuthenticationProvider implements AuthenticationProvider {
@@ -18,6 +22,12 @@ public class CustomeUsernamePasswordAuthenticationProvider implements Authentica
     private final UserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
 
+    /**
+     * 사용자가 제공한 로그인 정보를 이용하여 인증을 수행합니다.
+     * @param authentication 사용자가 제공한 인증 토큰
+     * @return 인증 성공 시 새로운 인증 토큰을 반환합니다.
+     * @throws AuthenticationException 인증 실패 시 예외를 던집니다.
+     */
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String email = authentication.getName();
@@ -30,6 +40,12 @@ public class CustomeUsernamePasswordAuthenticationProvider implements Authentica
         }
     }
 
+    /**
+     * 이 AuthenticationProvider가 지원하는 인증 토큰 타입을 반환합니다.
+     *
+     * @param authentication 인증 토큰 타입
+     * @return 지원하는 경우 true, 지원하지 않는 경우 false
+     */
     @Override
     public boolean supports(Class<?> authentication) {
         return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
