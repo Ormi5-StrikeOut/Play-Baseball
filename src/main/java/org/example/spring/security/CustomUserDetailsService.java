ba42f5de3f2c0ad00fafca7f8ce6ac13ab6ae172
LLSNsssz <seungjang93@gmail.com>
2024-08-30 17:14:50 +0900

    Docs: CustomUserDetailsService 자바독 작성

--- src/main/java/org/example/spring/security/CustomUserDetailsService.java
+++ src/main/java/org/example/spring/security/CustomUserDetailsService.java
@@ -14,12 +14,23 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+/**
+ * 사용자 인증 정보를 조회하는 사용자 정의 UserDetailsService 구현체 입니다.
+ * Spring Security에서 사용자 인증을 위해 필요한 UserDetails 객체를 제공합니다.
+ */
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
+    /**
+     * 사용자 이메일을 이용하여 사용자의 인증 정보를 조회합니다.
+     *
+     * @param username 사용자 이메일
+     * @return 사용자 인증 정보를 담은 UserDetails 객체
+     * @throws UsernameNotFoundException 해당 사용자를 찾을 수 없는 경우 발생
+     */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Member member = memberRepository.findByEmail(username)
