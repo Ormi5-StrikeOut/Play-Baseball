b2ec81d41a3c331a1b9716a440a3e57f5afa36f3
LLSNsssz <seungjang93@gmail.com>
2024-08-30 16:49:30 +0900

    Feat: CustomeUsernamePasswordAuthenticationProvider 구현
    - CustomUserDetailsService를 사용하여 사용자 인증을 처리하는 CustomeUsernamePasswordAuthenticationProvider 구현.
    - authenticate 메서드를 오버라이드하여 사용자의 이메일과 비밀번호를 기반으로 인증 수행.
    - PasswordEncoder를 사용하여 입력된 비밀번호와 저장된 비밀번호를 비교.
    - 인증 성공 시 UsernamePasswordAuthenticationToken을 반환하며, 인증 실패 시 BadCredentialsException을 발생시킴.
    - supports 메서드를 오버라이드하여 이 AuthenticationProvider가 UsernamePasswordAuthenticationToken을 지원하도록 설정.

---
+++ src/main/java/org/example/spring/security/CustomeUsernamePasswordAuthenticationProvider.java
@@ -0,0 +1,37 @@
+package org.example.spring.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomeUsernamePasswordAuthenticationProvider implements AuthenticationProvider {
+
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String email = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+        if (passwordEncoder.matches(password, userDetails.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(email, password, userDetails.getAuthorities());
+        } else {
+            throw new BadCredentialsException("잘못된 패스워드 입니다!");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
