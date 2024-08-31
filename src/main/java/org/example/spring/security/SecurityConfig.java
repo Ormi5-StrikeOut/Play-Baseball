43c007e78eb2833507c8c1785e73afee0ebb1149
LLSNsssz <seungjang93@gmail.com>
2024-09-01 04:34:31 +0900

    Feat: SecurityConfig JWT 기반 인증 구현
    - SecurityFilterChain 구성에 JwtValidatorFilter 추가하여 JWT 토큰 검증 기능 구현
    - 권한 규칙에 MemberRole 상수 사용

--- src/main/java/org/example/spring/security/SecurityConfig.java
+++ src/main/java/org/example/spring/security/SecurityConfig.java
@@ -2,41 +2,71 @@ package org.example.spring.security;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import org.example.spring.domain.member.MemberRole;
+import org.example.spring.security.jwt.CookieService;
+import org.example.spring.security.jwt.JwtAuthenticationService;
+import org.example.spring.security.jwt.JwtValidatorFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
+    private final CookieService cookieService;
+    private final JwtAuthenticationService jwtAuthenticationService;
+
+    public SecurityConfig(CookieService cookieService, JwtAuthenticationService jwtAuthenticationService) {
+        this.cookieService = cookieService;
+        this.jwtAuthenticationService = jwtAuthenticationService;
+    }
+
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .cors(withDefaults())
             .csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(request ->
-                request
-                    .anyRequest().permitAll()
+            .addFilterBefore(jwtValidatorFilter(), UsernamePasswordAuthenticationFilter.class)
+            .authorizeHttpRequests(request -> request
+                .requestMatchers("/api/members", "api/members/verify-role/").hasAuthority(MemberRole.ADMIN.name())
+                .requestMatchers("/api/exchanges").hasAuthority(MemberRole.USER.name())
+                .anyRequest().permitAll()
             );
         http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
+        http.httpBasic(basicConfig -> basicConfig.authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
         return http.build();
     }
 
     @Bean
-    PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @Bean
     public RoleHierarchy roleHierarchy() {
         return RoleHierarchyImpl.withDefaultRolePrefix()
-            .role("ADMIN").implies("USER")
+            .role(MemberRole.ADMIN.name()).implies(MemberRole.USER.name())
             .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public JwtValidatorFilter jwtValidatorFilter() {
+        return new JwtValidatorFilter(cookieService, jwtAuthenticationService);
     }
 }
