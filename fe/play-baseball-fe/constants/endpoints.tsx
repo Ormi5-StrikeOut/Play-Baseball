556c59b0891a6baa0583a6229c36dfa89230be23
The Shinee <teraburst@gmail.com>
2024-09-01 20:52:37 +0900

    #64 Refactor: update endpoints.tsx
    정의한 상수를 외부에서 사용할 수 있도록 모두 export 처리

--- fe/play-baseball-fe/constants/endpoints.tsx
+++ fe/play-baseball-fe/constants/endpoints.tsx
@@ -1,44 +1,44 @@
 const REQUEST_URL: string = "http://localhost:8080/api";
 
 // Member Endpoints
-const MEMBER: string = `${REQUEST_URL}/members`;
-const MEMBER_SIGNUP: string = `${MEMBER}/join`;
-const MEMBER_MODIFY: string = `${MEMBER}/{memberId}`;
-const MEMBER_VERIFY: string = `${MEMBER}/verify/{memberId}`;
-const MEMBER_RESIGN: string = `${MEMBER}/my`;
-const MEMBER_LOGIN: string = `${MEMBER}/login`;
-const MEMBER_GET_ALL: string = `${MEMBER}`;
-const MEMBER_REISSUE_TOKEN: string = `${REQUEST_URL}/auth/reissue-token/{memberId}`;
-const MEMBER_GET: string = `${MEMBER}/{memberId}`;
-const MEMBER_LOGOUT: string = `${REQUEST_URL}/logout`;
-const MEMBER_MODIFY_ROLE: string = `${MEMBER}/verify-role/{memberId}`;
+export const MEMBER: string = `${REQUEST_URL}/members`;
+export const MEMBER_SIGNUP: string = `${MEMBER}/join`;
+export const MEMBER_MODIFY: string = `${MEMBER}/{memberId}`;
+export const MEMBER_VERIFY: string = `${MEMBER}/verify/{memberId}`;
+export const MEMBER_RESIGN: string = `${MEMBER}/my`;
+export const MEMBER_LOGIN: string = `${MEMBER}/login`;
+export const MEMBER_GET_ALL: string = `${MEMBER}`;
+export const MEMBER_REISSUE_TOKEN: string = `${REQUEST_URL}/auth/reissue-token/{memberId}`;
+export const MEMBER_GET: string = `${MEMBER}/{memberId}`;
+export const MEMBER_LOGOUT: string = `${REQUEST_URL}/logout`;
+export const MEMBER_MODIFY_ROLE: string = `${MEMBER}/verify-role/{memberId}`;
 
 // Exchange Endpoints
-const EXCHANGE: string = `${REQUEST_URL}/exchanges`;
-const EXCHANGE_ADD: string = `${EXCHANGE}`;
-const EXCHANGE_MODIFY: string = `${EXCHANGE}/{id}`;
-const EXCHANGE_DELETE: string = `${EXCHANGE}/{id}`;
-const EXCHANGE_GET_ALL: string = `${EXCHANGE}`;
-const EXCHANGE_GET_LATEST_FIVE: string = `${EXCHANGE}/five`;
-const EXCHANGE_MODIFY_SALES_STATUS: string = `${EXCHANGE}/{id}`;
-const EXCHANGE_GET_MY: string = `${EXCHANGE}/{memberId}`;
+export const EXCHANGE: string = `${REQUEST_URL}/exchanges`;
+export const EXCHANGE_ADD: string = `${EXCHANGE}`;
+export const EXCHANGE_MODIFY: string = `${EXCHANGE}/{id}`;
+export const EXCHANGE_DELETE: string = `${EXCHANGE}/{id}`;
+export const EXCHANGE_GET_ALL: string = `${EXCHANGE}`;
+export const EXCHANGE_GET_LATEST_FIVE: string = `${EXCHANGE}/five`;
+export const EXCHANGE_MODIFY_SALES_STATUS: string = `${EXCHANGE}/{id}`;
+export const EXCHANGE_GET_MY: string = `${EXCHANGE}/{memberId}`;
 
 // Review Endpoints
-const REVIEW: string = `${REQUEST_URL}/reviews`;
-const REVIEW_ADD: string = `${REVIEW}`;
-const REVIEW_MODIFY: string = `${REVIEW}/{id}`;
-const REVIEW_GET_MEMBER: string = `${REVIEW}/member/{id}`;
-const REVIEW_GET_MY: string = `${REVIEW}/my`;
+export const REVIEW: string = `${REQUEST_URL}/reviews`;
+export const REVIEW_ADD: string = `${REVIEW}`;
+export const REVIEW_MODIFY: string = `${REVIEW}/{id}`;
+export const REVIEW_GET_MEMBER: string = `${REVIEW}/member/{id}`;
+export const REVIEW_GET_MY: string = `${REVIEW}/my`;
 
 // Like Endpoints
-const LIKE: string = `${REQUEST_URL}/exchange-likes`;
-const LIKE_ADD: string = `${LIKE}/{postId}`;
-const LIKE_DELETE: string = `${LIKE}/{postId}`;
-const LIKE_GET_MY: string = `${LIKE}/{userId}`;
+export const LIKE: string = `${REQUEST_URL}/exchange-likes`;
+export const LIKE_ADD: string = `${LIKE}/{postId}`;
+export const LIKE_DELETE: string = `${LIKE}/{postId}`;
+export const LIKE_GET_MY: string = `${LIKE}/{userId}`;
 
 // Chat Endpoints
-const CHATROOM: string = `${REQUEST_URL}/chatrooms`;
-const CHATROOM_ADD: string = `${CHATROOM}`;
-const CHATROOM_DELETE: string = `${CHATROOM}`;
-const CHAT: string = `${REQUEST_URL}/chat`;
-const CHAT_POST_MESSAGE: string = `${CHAT}`;
+export const CHATROOM: string = `${REQUEST_URL}/chatrooms`;
+export const CHATROOM_ADD: string = `${CHATROOM}`;
+export const CHATROOM_DELETE: string = `${CHATROOM}`;
+export const CHAT: string = `${REQUEST_URL}/chat`;
+export const CHAT_POST_MESSAGE: string = `${CHAT}`;
