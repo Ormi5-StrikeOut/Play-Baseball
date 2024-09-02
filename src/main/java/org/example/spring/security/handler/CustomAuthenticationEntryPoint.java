3efe9284046f7ac8ce21d57aa6f27bfa49d3b252
LLSNsssz <seungjang93@gmail.com>
2024-09-02 09:55:55 +0900

    Rename: security 구조 변경

--- src/main/java/org/example/spring/security/handler/CustomAuthenticationEntryPoint.java
+++ src/main/java/org/example/spring/security/handler/CustomAuthenticationEntryPoint.java
@@ -1,4 +1,4 @@
-package org.example.spring.security;
+package org.example.spring.security.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
