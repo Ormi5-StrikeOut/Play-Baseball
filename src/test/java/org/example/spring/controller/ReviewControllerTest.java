91788b326e11451e7d554d2e99e757740a2b2940
LLSNsssz <seungjang93@gmail.com>
2024-09-03 10:37:54 +0900

    Test: ReviewControllerTest 사용되지 않는 import문 제거

--- src/test/java/org/example/spring/controller/ReviewControllerTest.java
+++ src/test/java/org/example/spring/controller/ReviewControllerTest.java
@@ -33,7 +33,6 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
