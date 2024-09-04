4ffa2bf810f5d681bcb01c9240d56f1586eeb21b
The Shinee <teraburst@gmail.com>
2024-09-04 17:10:29 +0900

    #131 Fix: headers compile error

--- src/main/java/org/example/spring/security/config/SecurityConfig.java
+++ src/main/java/org/example/spring/security/config/SecurityConfig.java
@@ -26,6 +26,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -90,7 +91,7 @@ public class SecurityConfig {
                         .maxAgeInSeconds(31536000)
 
                 )
-                .referrerPolicy(referrer -> referrer.policy(ReferrerPolicy.ORIGIN))
+                .referrerPolicy(referrer -> referrer.policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.ORIGIN))
         );
 
         return http.build();
