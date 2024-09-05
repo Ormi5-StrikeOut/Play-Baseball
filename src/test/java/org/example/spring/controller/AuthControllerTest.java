68e70dedd725e156ec78df0bdb82b5bb3c35ea2a
LLSNsssz <seungjang93@gmail.com>
2024-09-05 10:17:59 +0900

    Test: AuthControllerTest 수정

--- src/test/java/org/example/spring/controller/AuthControllerTest.java
+++ src/test/java/org/example/spring/controller/AuthControllerTest.java
@@ -20,12 +20,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
-@ActiveProfiles("dev")
 class AuthControllerTest {
 
     @Autowired
