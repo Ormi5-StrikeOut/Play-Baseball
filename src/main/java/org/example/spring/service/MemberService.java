fae252bd5a1a6e810ca7c0ef8bac182d18409926
LLSNsssz <seungjang93@gmail.com>
2024-09-05 22:42:21 +0900

    Feat: MemberService getMyMember 회원단건조회 메서드 추가

--- src/main/java/org/example/spring/service/MemberService.java
+++ src/main/java/org/example/spring/service/MemberService.java
@@ -3,6 +3,7 @@ package org.example.spring.service;
 import static org.example.spring.domain.member.dto.MemberJoinRequestDto.toEntity;
 import static org.example.spring.domain.member.dto.MemberResponseDto.toDto;
 
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
@@ -10,18 +11,20 @@ import lombok.RequiredArgsConstructor;
 import org.example.spring.domain.member.Member;
 import org.example.spring.domain.member.dto.MemberJoinRequestDto;
 import org.example.spring.domain.member.dto.MemberResponseDto;
+import org.example.spring.exception.ResourceNotFoundException;
 import org.example.spring.repository.MemberRepository;
+import org.example.spring.security.jwt.JwtTokenValidator;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * 회원 관련 서비스 로직을 담당하는 클래스입니다.
- * 회원 가입, 회원 목록 조회 등의 기능을 제공합니다.
+ * 회원 관련 서비스 로직을 담당하는 클래스입니다. 회원 가입, 회원 목록 조회 등의 기능을 제공합니다.
  */
 @Service
 @RequiredArgsConstructor
@@ -30,21 +33,32 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtTokenValidator jwtValidator;
 
-    @Getter
-    private enum MemberField {
-        EMAIL("이메일"),
-        PASSWORD("비밀번호"),
-        NICKNAME("닉네임"),
-        NAME("이름"),
-        PHONE_NUMBER("전화번호"),
-        GENDER("성별");
+    /**
+     * 페이징 처리된 전체 회원 목록을 조회합니다.
+     *
+     * @param page 조회할 페이지 번호
+     * @param size 한 페이지당 표시할 회원 수
+     * @return 페이징된 회원 목록 DTO
+     */
+    @Transactional(readOnly = true)
+    public Page<MemberResponseDto> getAllMembers(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("id").descending());
+        Page<Member> members = memberRepository.findAll(pageable);
+        return members.map(MemberResponseDto::toDto);
+    }
 
-        private final String description;
-
-        MemberField(String description) {
-            this.description = description;
+    @Transactional(readOnly = true)
+    public MemberResponseDto getMyMember(HttpServletRequest request) {
+        String token = jwtValidator.extractTokenFromHeader(request);
+        UserDetails userDetails = jwtValidator.getUserDetails(token);
+        String email = jwtValidator.extractUsername(token);
+        if (userDetails.getUsername().equals(email)) {
+            Member member = memberRepository.findByEmail(email).orElseThrow(() -> new ResourceNotFoundException("Member", "email", email));
+            return MemberResponseDto.toDto(member);
         }
+        return null;
     }
 
     /**
@@ -65,20 +79,6 @@ public class MemberService {
         return toDto(saveMember);
     }
 
-    /**
-     * 페이징 처리된 전체 회원 목록을 조회합니다.
-     *
-     * @param page 조회할 페이지 번호
-     * @param size 한 페이지당 표시할 회원 수
-     * @return 페이징된 회원 목록 DTO
-     */
-    @Transactional(readOnly = true)
-    public Page<MemberResponseDto> getAllMembers(int page, int size) {
-        Pageable pageable = PageRequest.of(page, size, Sort.by("id").descending());
-        Page<Member> members= memberRepository.findAll(pageable);
-        return members.map(MemberResponseDto::toDto);
-    }
-
     private void checkDuplicates(MemberJoinRequestDto memberJoinRequestDto) {
         List<String> errorMessages = new ArrayList<>();
 
@@ -96,6 +96,22 @@ public class MemberService {
 
         if (!errorMessages.isEmpty()) {
             throw new IllegalArgumentException("이미 존재하는 " + String.join(", ", errorMessages) + "입니다.");
+        }
+    }
+
+    @Getter
+    private enum MemberField {
+        EMAIL("이메일"),
+        PASSWORD("비밀번호"),
+        NICKNAME("닉네임"),
+        NAME("이름"),
+        PHONE_NUMBER("전화번호"),
+        GENDER("성별");
+
+        private final String description;
+
+        MemberField(String description) {
+            this.description = description;
         }
     }
 }
