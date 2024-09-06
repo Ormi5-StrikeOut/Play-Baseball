93ee73e2ba2b743a327319690a11e951a23ab7a5
The Shinee <teraburst@gmail.com>
2024-09-06 14:40:56 +0900

    #166 Refactor: Update ExchangeRepository
    - 게시글 목록을 페이지별로 조회해야 할 메서드들은 List가 아닌 Page를 사용합니다.

--- src/main/java/org/example/spring/repository/ExchangeRepository.java
+++ src/main/java/org/example/spring/repository/ExchangeRepository.java
@@ -1,6 +1,8 @@
 package org.example.spring.repository;
 
 import org.example.spring.domain.exchange.Exchange;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,10 +11,14 @@ import java.util.List;
 @Repository
 public interface ExchangeRepository extends JpaRepository<Exchange, Long> {
 
+    Page<Exchange> findAll(Pageable pageable);
+
+    // 검색 키워드에 포함되어 있는 게시글 목록 조회
+    Page<Exchange> findByTitleContaining(String title, Pageable pageable);
+
     // 최근 5개의 게시글 조회
     List<Exchange> findTop5ByOrderByCreatedAtDesc();
 
     // 특정 회원이 작성한 게시글 조회
-    List<Exchange> findByWriterId(Long memberId);
-
+    Page<Exchange> findByWriterId(Long memberId, Pageable pageable);
 }
