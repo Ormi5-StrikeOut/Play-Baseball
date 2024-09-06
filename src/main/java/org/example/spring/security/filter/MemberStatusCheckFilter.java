78259ea7ac4c0207aee7a58731d8c44a2ccfb011
LLSNsssz <seungjang93@gmail.com>
2024-09-06 14:46:26 +0900

    Refactor: 사용되지 않는 import문 제거

--- src/main/java/org/example/spring/security/filter/MemberStatusCheckFilter.java
+++ src/main/java/org/example/spring/security/filter/MemberStatusCheckFilter.java
@@ -15,7 +15,6 @@ import org.example.spring.exception.AccountDeletedException;
 import org.example.spring.security.jwt.JwtTokenValidator;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
