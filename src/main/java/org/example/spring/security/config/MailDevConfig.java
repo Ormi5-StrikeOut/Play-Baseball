5958746119f2808f278f5ed7ce6e9e495c9878a3
LLSNsssz <seungjang93@gmail.com>
2024-09-07 19:39:12 +0900

    Feat: MailConfig JavaMailSender 빈 구성

---
+++ src/main/java/org/example/spring/security/config/MailDevConfig.java
@@ -0,0 +1,30 @@
+package org.example.spring.security.config;
+
+import java.util.Properties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+@Profile("dev")
+public class MailDevConfig {
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("your-email@gmail.com");
+        mailSender.setPassword("your-app-password");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
