2a67ad95ee42f78e35b3c0407d5a65458d7bfab8
LLSNsssz <seungjang93@gmail.com>
2024-09-07 22:26:00 +0900

    Fix: 스프링 부트 테스트 어노테이션 속성 명시

--- src/test/java/org/example/spring/ApplicationTests.java
+++ src/test/java/org/example/spring/ApplicationTests.java
@@ -2,8 +2,9 @@ package org.example.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = ApplicationTests.class)
 class ApplicationTests {
 
 	@Test
