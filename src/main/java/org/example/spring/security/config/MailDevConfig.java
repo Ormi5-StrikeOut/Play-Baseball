dff929499c891154da713e79651de13280abb861
LLSNsssz <seungjang93@gmail.com>
2024-09-07 21:03:39 +0900

    Chore: 메일 발송 관련 환경변수 조정

--- src/main/java/org/example/spring/security/config/MailDevConfig.java
+++ src/main/java/org/example/spring/security/config/MailDevConfig.java
@@ -1,6 +1,8 @@
 package org.example.spring.security.config;
 
 import java.util.Properties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,23 +10,60 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
+@RequiredArgsConstructor
 @Profile("dev")
 public class MailDevConfig {
+
+    private static final String MAIL_SMTP_AUTH = "mail.smtp.auth";
+    private static final String MAIL_DEBUG = "mail.smtp.debug";
+    private static final String MAIL_CONNECTION_TIMEOUT = "mail.smtp.connectiontimeout";
+    private static final String MAIL_SMTP_STARTTLS_ENABLE = "mail.smtp.starttls.enable";
+
+    // SMTP 서버
+    @Value("${spring.mail.host}")
+    private String host;
+
+    // 계정
+    @Value("${spring.mail.username}")
+    private String username;
+
+    // 비밀번호
+    @Value("${spring.mail.password}")
+    private String password;
+
+    // 포트번호
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.properties.mail.smtp.auth}")
+    private boolean auth;
+
+    @Value("${spring.mail.properties.mail.smtp.debug}")
+    private boolean debug;
+
+    @Value("${spring.mail.properties.mail.smtp.connectiontimeout}")
+    private int connectionTimeout;
+
+    @Value("${spring.mail.properties.mail.starttls.enable}")
+    private boolean startTlsEnable;
+
     @Bean
     public JavaMailSender javaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost(host);
+        javaMailSender.setUsername(username);
+        javaMailSender.setPassword(password);
+        javaMailSender.setPort(port);
 
-        mailSender.setUsername("your-email@gmail.com");
-        mailSender.setPassword("your-app-password");
+        Properties properties = javaMailSender.getJavaMailProperties();
+        properties.put(MAIL_SMTP_AUTH, auth);
+        properties.put(MAIL_DEBUG, debug);
+        properties.put(MAIL_CONNECTION_TIMEOUT, connectionTimeout);
+        properties.put(MAIL_SMTP_STARTTLS_ENABLE, startTlsEnable);
 
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
+        javaMailSender.setJavaMailProperties(properties);
+        javaMailSender.setDefaultEncoding("UTF-8");
 
-        return mailSender;
+        return javaMailSender;
     }
 }
