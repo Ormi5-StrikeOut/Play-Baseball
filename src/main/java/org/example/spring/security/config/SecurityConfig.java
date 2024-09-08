2a4bae167cad86f8ebf9e62f921095e8f556bf29
LLSNsssz <seungjang93@gmail.com>
2024-09-09 01:40:32 +0900

    Refactor: SecurityConfig 권한 재설정 및 사용하지 않는 메서드 제거

--- src/main/java/org/example/spring/security/config/SecurityConfig.java
+++ src/main/java/org/example/spring/security/config/SecurityConfig.java
@@ -17,19 +17,15 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authorization.AuthorizationDecision;
-import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.XXssConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -61,7 +57,8 @@ public class SecurityConfig {
             .addFilterAfter(memberStatusCheckFilter, JwtValidatorFilter.class)
             .authorizeHttpRequests(request -> request
                 // 비회원 공개 엔드포인트
-                .requestMatchers("/", "/api/auth/login", "/swagger-ui/**", "/v3/api-docs/**", "/v3/api-docs/swagger-config").permitAll()
+                .requestMatchers("/", "/api/auth/login", "/swagger-ui/**", "/v3/api-docs/**", "/v3/api-docs/swagger-config", "/favicon.ico")
+                .permitAll()
                 .requestMatchers("/api/members/reset-password", "/api/members/request-password-reset").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/members/join").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/members/verify-email").permitAll()
@@ -69,19 +66,18 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.GET, "/api/reviews").permitAll()
 
                 // 사용자 및 관리자 엔드포인트 (이메일 인증 필요)
-                .requestMatchers(HttpMethod.PUT, "/api/members/**").access(hasRolesAndEmailVerified(MemberRole.USER, MemberRole.ADMIN))
-                .requestMatchers(HttpMethod.POST, "/api/exchanges", "/api/reviews").access(hasRolesAndEmailVerified(MemberRole.USER, MemberRole.ADMIN))
-                .requestMatchers(HttpMethod.PUT, "/api/exchanges/**", "/api/reviews/**").access(hasRolesAndEmailVerified(MemberRole.USER, MemberRole.ADMIN))
-                .requestMatchers("/api/messages").access(hasRolesAndEmailVerified(MemberRole.USER, MemberRole.ADMIN))
-                .requestMatchers("/api/messages/**").access(hasRolesAndEmailVerified(MemberRole.USER, MemberRole.ADMIN))
+                .requestMatchers(HttpMethod.PUT, "/api/members/**").hasAuthority(MemberRole.VERIFIED_USER.name())
+                .requestMatchers(HttpMethod.POST, "/api/exchanges", "/api/reviews").hasAuthority(MemberRole.VERIFIED_USER.name())
+                .requestMatchers(HttpMethod.PUT, "/api/exchanges/**", "/api/reviews/**").hasAuthority(MemberRole.VERIFIED_USER.name())
+                .requestMatchers("/api/messages", "/api/messages/**").hasAuthority(MemberRole.VERIFIED_USER.name())
 
-                // 관리자 전용 엔드포인트 (이메일 인증 필요)
-                .requestMatchers(HttpMethod.GET, "/api/members").access(hasRoleAndEmailVerified(MemberRole.ADMIN))
-                .requestMatchers(HttpMethod.PUT, "/api/members/verify-role/**").access(hasRoleAndEmailVerified(MemberRole.ADMIN))
-
+                // 관리자 전용 엔드포인트
+                .requestMatchers(HttpMethod.GET, "/api/members").hasAnyAuthority(MemberRole.ADMIN.name())
+                .requestMatchers(HttpMethod.PUT, "/api/members/verify-role/**").hasAnyAuthority(MemberRole.ADMIN.name())
 
                 // 인증된 사용자 엔드포인트 (이메일 인증 불필요)
-                .requestMatchers("/api/auth/logout", "/api/members/my/**", "/api/members/my", "/api/members/resend-verification-email").authenticated()
+                .requestMatchers("/api/auth/logout", "/api/members/my/**", "/api/members/my", "/api/members/resend-verification-email")
+                .authenticated()
                 .requestMatchers("/api/exchange-likes/**").authenticated()
 
                 // 기타 모든 요청
@@ -107,42 +103,9 @@ public class SecurityConfig {
             )
             .referrerPolicy(referrer -> referrer
                 .policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN)
-            )        );
+            ));
 
         return http.build();
-    }
-
-    private AuthorizationManager<RequestAuthorizationContext> hasRolesAndEmailVerified(MemberRole... roles) {
-        return (authentication, context) -> {
-            if (authentication == null) {
-                return new AuthorizationDecision(false);
-            }
-
-            boolean hasRequiredRole = false;
-            for (MemberRole role : roles) {
-                if (authentication.get().getAuthorities().contains(new SimpleGrantedAuthority("ROLE_" + role.name()))) {
-                    hasRequiredRole = true;
-                    break;
-                }
-            }
-
-            boolean isEmailVerified = authentication.get().getAuthorities().contains(new SimpleGrantedAuthority("EMAIL_VERIFIED"));
-
-            return new AuthorizationDecision(hasRequiredRole && isEmailVerified);
-        };
-    }
-
-    private AuthorizationManager<RequestAuthorizationContext> hasRoleAndEmailVerified(MemberRole role) {
-        return (authentication, context) -> {
-            if (authentication == null) {
-                return new AuthorizationDecision(false);
-            }
-
-            boolean hasRequiredRole = authentication.get().getAuthorities().contains(new SimpleGrantedAuthority("ROLE_" + role.name()));
-            boolean isEmailVerified = authentication.get().getAuthorities().contains(new SimpleGrantedAuthority("EMAIL_VERIFIED"));
-
-            return new AuthorizationDecision(hasRequiredRole && isEmailVerified);
-        };
     }
 
     @Bean
