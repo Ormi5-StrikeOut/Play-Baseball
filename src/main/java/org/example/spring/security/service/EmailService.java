99334dcc48a785f47f287d7da30e1d9998210bcc
LLSNsssz <seungjang93@gmail.com>
2024-09-08 15:01:38 +0900

    Refactor: 환경 분리를 위한 base-url 추가

--- src/main/java/org/example/spring/security/service/EmailService.java
+++ src/main/java/org/example/spring/security/service/EmailService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
 import org.example.spring.security.utils.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,9 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
     private final JwtUtils jwtUtils;
+
+    @Value("${app.base-url}")
+    private String baseUrl;
 
     @Autowired
     public EmailService(JavaMailSender mailSender, JwtUtils jwtUtils) {
@@ -26,10 +30,9 @@ public class EmailService {
         message.setTo(email);
         message.setSubject("이메일 인증");
         message.setText("다음 링크를 클릭하여 이메일을 인증하세요: " +
-            "https://api.ioshane.com/api/members/verify-email?token=" + token);
+            baseUrl + "/api/members/verify-email?token=" + token);
         mailSender.send(message);
     }
-
 
     public String generateEmailVerificationToken(String email) {
         return Jwts.builder()
