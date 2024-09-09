e15473a607ba7218c9d467fcb291499bca934451
LLSNsssz <seungjang93@gmail.com>
2024-09-09 09:31:51 +0900

    Refactor: GlobalExceptionHandler 로깅 수정

--- src/main/java/org/example/spring/common/GlobalExceptionHandler.java
+++ src/main/java/org/example/spring/common/GlobalExceptionHandler.java
@@ -25,41 +25,42 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ApiResponseDto<Void>> handleIllegalArgumentException(IllegalArgumentException e) {
+        log.debug("IllegalArgument: " + e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
             .body(ApiResponseDto.error("요청에 실패했습니다: " + e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponseDto<Void>> handleException(Exception e) {
-        log.debug("서버 오류 발생: {}", String.valueOf(e));
+        log.debug("서버 오류 발생: " + e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body(ApiResponseDto.error("서버 오류가 발생했습니다."));
     }
 
     @ExceptionHandler(AccountDeletedException.class)
     public ResponseEntity<ApiResponseDto<Void>> handleAccountDeletedException(AccountDeletedException ex) {
-        log.debug("Attempt to access deleted account: {}", ex.getMessage());
+        log.debug("Attempt to access deleted account: " + ex);
         return ResponseEntity.status(HttpStatus.FORBIDDEN)
             .body(ApiResponseDto.error("이 계정은 삭제 되었습니다"));
     }
 
     @ExceptionHandler(AccountBannedException.class)
     public ResponseEntity<ApiResponseDto<Void>> handleAccountBannedException(AccountBannedException ex) {
-        log.debug("Attempt to access banned account: {}", ex.getMessage());
+        log.debug("Attempt to access banned account: " + ex);
         return ResponseEntity.status(HttpStatus.FORBIDDEN)
             .body(ApiResponseDto.error("이 계정은 밴 되었습니다"));
     }
 
     @ExceptionHandler(InvalidCredentialsException.class)
     public ResponseEntity<ApiResponseDto<Void>> handleInvalidCredentialsException(InvalidCredentialsException ex) {
-        log.debug("Invalid credentials: {}", ex.getMessage());
+        log.debug("Invalid credentials: " + ex);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
             .body(ApiResponseDto.error("Invalid email or password"));
     }
 
     @ExceptionHandler(InvalidTokenException.class)
     public ResponseEntity<ApiResponseDto<Void>> handleInvalidTokenException(InvalidTokenException ex) {
-        log.debug("Invalid token: {}", ex.getMessage());
+        log.debug("Invalid token: " + ex);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
             .body(ApiResponseDto.error("토큰이 유효하지 않습니다 " + ex.getMessage()));
     }
@@ -80,19 +81,19 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(EmailVerificationTokenExpiredException.class)
     public ResponseEntity<ApiResponseDto<Void>> handleExpiredTokenException(EmailVerificationTokenExpiredException e) {
-        log.debug("email verify token is expired: {}", e.getMessage());
+        log.debug("email verify token is expired: " + e);
         return ResponseEntity.status(HttpStatus.GONE).body(ApiResponseDto.error("이메일 인증 토큰이 만료되었습니다. 새로운 인증 이메일을 요청해주세요."));
     }
 
     @ExceptionHandler(MemberNotFoundException.class)
     public ResponseEntity<ApiResponseDto<Void>> handleMemberNotFoundException(MemberNotFoundException e) {
-        log.debug("Member not found: {}", e.getMessage());
+        log.debug("Member not found: " + e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponseDto.error("회원을 찾지 못했습니다"));
     }
 
     @ExceptionHandler(EmailAlreadyVerifiedException.class)
     public ResponseEntity<ApiResponseDto<Void>> handleEmailAlreadyVerifiedException(EmailAlreadyVerifiedException e) {
-        log.debug("is already verified email: {}", e.getMessage());
+        log.debug("is already verified email: " + e);
         return ResponseEntity.badRequest().body(ApiResponseDto.error("이미 인증된 이메일입니다."));
     }
 
@@ -116,7 +117,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(AuthenticationFailedException.class)
     public ResponseEntity<ApiResponseDto<Void>> handleAuthenticationFailedException(AuthenticationFailedException ex) {
-        log.error("Authentication Failed: " + ex.getMessage());
+        log.error("Authentication Failed: " + ex);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
             .body(ApiResponseDto.error("인증에 실패 했습니다."));
     }
