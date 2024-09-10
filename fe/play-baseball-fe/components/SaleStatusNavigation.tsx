0c8f84ef31df2c79fb394060c2374c07d0458365
The Shinee <teraburst@gmail.com>
2024-09-11 00:54:07 +0900

    #239 Feat: 필터 네비게이션 활성화 효과 및 활성화된 버튼 시 필터 해제하도록 기능 추가

--- fe/play-baseball-fe/components/SaleStatusNavigation.tsx
+++ fe/play-baseball-fe/components/SaleStatusNavigation.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Box,
   Typography,
@@ -12,8 +12,23 @@ import qs from "qs";
 
 const SaleStatusNavigation: React.FC = () => {
   const router = useRouter();
+  const [selectedStatus, setSelectedStatus] = useState<string | null>(null);
+
+  // 현재 URL의 쿼리에서 status 값 가져오기
+  useEffect(() => {
+    const { status } = router.query;
+    if (status === "SALE" || status === "COMPLETE") {
+      setSelectedStatus(status as string);
+    } else {
+      setSelectedStatus(null); // status가 없거나 NONE일 때
+    }
+  }, [router.query]);
 
   const handleNavigation = (status: string) => {
+    if (selectedStatus === status) {
+      // 이미 선택된 상태인 경우 "NONE"으로 변경
+      status = "NONE";
+    }
     const updatedQuery = { ...router.query, status };
     const url = `${window.location.pathname}?${qs.stringify(updatedQuery)}`;
 
@@ -46,7 +61,15 @@ const SaleStatusNavigation: React.FC = () => {
       {/* 판매 상태 리스트 */}
       <List>
         {/* 판매중 상태 */}
-        <ListItemButton onClick={() => handleNavigation("SALE")}>
+        <ListItemButton
+          onClick={() => handleNavigation("SALE")}
+          sx={{
+            bgcolor: selectedStatus === "SALE" ? "#E0E0E0" : "transparent", // 선택된 경우 배경색 적용
+            "&:hover": {
+              bgcolor: "#F5F5F5",
+            },
+          }}
+        >
           <ListItemText
             primary={
               <Typography variant="h6" color="textPrimary">
@@ -63,7 +86,15 @@ const SaleStatusNavigation: React.FC = () => {
         <Divider />
 
         {/* 판매완료 상태 */}
-        <ListItemButton onClick={() => handleNavigation("COMPLETE")}>
+        <ListItemButton
+          onClick={() => handleNavigation("COMPLETE")}
+          sx={{
+            bgcolor: selectedStatus === "COMPLETE" ? "#E0E0E0" : "transparent", // 선택된 경우 배경색 적용
+            "&:hover": {
+              bgcolor: "#F5F5F5",
+            },
+          }}
+        >
           <ListItemText
             primary={
               <Typography variant="h6" color="textPrimary">
