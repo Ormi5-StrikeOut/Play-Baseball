3589aa56f6bdacb26b37ae08573da488f72c337f
LLSNsssz <seungjang93@gmail.com>
2024-09-12 20:23:46 +0900

    Docs: MailConfig 자바독 추가

--- src/main/java/org/example/spring/security/config/MailConfig.java
+++ src/main/java/org/example/spring/security/config/MailConfig.java
@@ -1,6 +1,7 @@
 package org.example.spring.security.config;
 
 import java.util.Properties;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,60 +9,65 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+/**
+ * 메일 서버 설정을 위한 구성 클래스입니다.
+ * 개발 환경(prod) 프로필에서 활성화됩니다.
+ */
+
 @Configuration
-@Profile("!dev")
+@Profile("prod")
 public class MailConfig {
-    private static final String MAIL_SMTP_AUTH = "mail.smtp.auth";
-    private static final String MAIL_DEBUG = "mail.smtp.debug";
-    private static final String MAIL_CONNECTION_TIMEOUT = "mail.smtp.connectiontimeout";
-    private static final String MAIL_SMTP_STARTTLS_ENABLE = "mail.smtp.starttls.enable";
+	private static final String MAIL_SMTP_AUTH = "mail.smtp.auth";
+	private static final String MAIL_DEBUG = "mail.smtp.debug";
+	private static final String MAIL_CONNECTION_TIMEOUT = "mail.smtp.connectiontimeout";
+	private static final String MAIL_SMTP_STARTTLS_ENABLE = "mail.smtp.starttls.enable";
 
-    // SMTP 서버
-    @Value("${spring.mail.host}")
-    private String host;
+	// SMTP 서버
+	@Value("${spring.mail.host}")
+	private String host;
 
-    // 계정
-    @Value("${spring.mail.username}")
-    private String username;
+	// 계정
+	@Value("${spring.mail.username}")
+	private String username;
 
-    // 비밀번호
-    @Value("${spring.mail.password}")
-    private String password;
+	// 비밀번호
+	@Value("${spring.mail.password}")
+	private String password;
 
-    // 포트번호
-    @Value("${spring.mail.port}")
-    private int port;
+	// 포트번호
+	@Value("${spring.mail.port}")
+	private int port;
 
-    @Value("${spring.mail.properties.mail.smtp.auth}")
-    private boolean auth;
+	@Value("${spring.mail.properties.mail.smtp.auth}")
+	private boolean auth;
 
-    @Value("${spring.mail.properties.mail.smtp.debug}")
-    private boolean debug;
+	@Value("${spring.mail.properties.mail.smtp.debug}")
+	private boolean debug;
 
-    @Value("${spring.mail.properties.mail.smtp.connectiontimeout}")
-    private int connectionTimeout;
+	@Value("${spring.mail.properties.mail.smtp.connectiontimeout}")
+	private int connectionTimeout;
 
-    @Value("${spring.mail.properties.mail.starttls.enable}")
-    private boolean startTlsEnable;
+	@Value("${spring.mail.properties.mail.starttls.enable}")
+	private boolean startTlsEnable;
 
-    @Bean
-    public JavaMailSender javaMailSender() {
-        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-        javaMailSender.setHost(host);
-        javaMailSender.setUsername(username);
-        javaMailSender.setPassword(password);
-        javaMailSender.setPort(port);
+	@Bean
+	public JavaMailSender javaMailSender() {
+		JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+		javaMailSender.setHost(host);
+		javaMailSender.setUsername(username);
+		javaMailSender.setPassword(password);
+		javaMailSender.setPort(port);
 
-        Properties properties = javaMailSender.getJavaMailProperties();
-        properties.put(MAIL_SMTP_AUTH, auth);
-        properties.put(MAIL_DEBUG, debug);
-        properties.put(MAIL_CONNECTION_TIMEOUT, connectionTimeout);
-        properties.put(MAIL_SMTP_STARTTLS_ENABLE, startTlsEnable);
+		Properties properties = javaMailSender.getJavaMailProperties();
+		properties.put(MAIL_SMTP_AUTH, auth);
+		properties.put(MAIL_DEBUG, debug);
+		properties.put(MAIL_CONNECTION_TIMEOUT, connectionTimeout);
+		properties.put(MAIL_SMTP_STARTTLS_ENABLE, startTlsEnable);
 
-        javaMailSender.setJavaMailProperties(properties);
-        javaMailSender.setDefaultEncoding("UTF-8");
+		javaMailSender.setJavaMailProperties(properties);
+		javaMailSender.setDefaultEncoding("UTF-8");
 
-        return javaMailSender;
-    }
+		return javaMailSender;
+	}
 }
 
